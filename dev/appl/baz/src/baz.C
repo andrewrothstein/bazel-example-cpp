46cf7f6c9ac5263dacaccfdbefa3efd818f3f629
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-16 23:00:31 -0500

    an include specific to an application

--- dev/appl/baz/src/baz.C
+++ dev/appl/baz/src/baz.C
@@ -1,13 +1,13 @@
 #include <string>
 
+#include "baz/Baz.H"
 #include "foo/Foo.H"
 #include "bar/Bar.H"
 
 int
 main(int argc, const char ** argv) {
-  const Monster * m(argc > 1 ? new Foo("Cecelia") : new Bar(14));
+  const Monster * m(argc > 1 ? new Foo("Cecelia") : new Bar(baz.Baz.Param));
   m->awaken(std::cout);
   delete m;
   return EXIT_SUCCESS;
 }
-  
